a4053042506c7e8f6b03025bf547fe82406ac0c4
Bruce Snyder <bruce.snyder@gmail.com>
2011-06-07 12:14:53 -0600

    Initial add

---
+++ src/main/java/org/bsnyder/spring/amqp/ProducerApp.java
@@ -0,0 +1,26 @@
+package org.bsnyder.spring.amqp;
+
+import org.bsnyder.spring.amqp.producer.SimpleMessageProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ProducerApp {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ProducerApp.class);
+	
+	public static void main(String[] args) {
+		String sendType = null;
+
+		if (args.length > 0 && null != args[0] && !"".equals(args[0])) {
+			sendType = args[0];
+		}
+
+		ApplicationContext context = 
+			new ClassPathXmlApplicationContext("/META-INF/spring/producer-amqp-context.xml", ProducerApp.class);
+		SimpleMessageProducer producer = (SimpleMessageProducer) context.getBean("messageProducer");
+		LOG.debug("Using the '{}' sendType", sendType);
+		producer.sendMessages(sendType);
+	}
+}
